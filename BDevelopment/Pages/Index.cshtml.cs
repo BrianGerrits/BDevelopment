f200462a06e5afa3b7ee0d2b03786804474397b1
Brian Gerrits <bgerrits@edu.bartimeus.nl>
2020-03-03 11:32:39 +0100

    Add project files.

---
+++ BDevelopment/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Octokit;
+
+namespace BDevelopment.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
