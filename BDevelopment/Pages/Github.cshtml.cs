e27f94eab7f1fcc2c18408b95a5e1a6e761d1760
Brian Gerrits <bgerrits@edu.bartimeus.nl>
2020-03-10 11:55:13 +0100

    Added Github API, to fetch my repository data and display it on my website's front-end

---
+++ BDevelopment/Pages/Github.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using GithubConverter;
+using Newtonsoft.Json;
+using System.Reflection;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace RazorWebPagesApplication.Pages
+{
+    public class GithubModel : PageModel
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public async Task OnGet() { 
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", "Repository Reporter");
+
+            var stringTask = client.GetStringAsync("https://api.github.com/users/BrianGerrits/repos");
+
+            var msg = await stringTask;
+            var data = GithubData.FromJson(msg);
+            ViewData["data"] = data;
+
+        }
+    } //end class
+}
